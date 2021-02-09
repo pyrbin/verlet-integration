b2278685a468d2fa3cf5a3576e8d2c2e8fd425a3
Frans <31902695+pyrbin@users.noreply.github.com>
2021-02-09 23:33:25 +0100

    Added delegate parameter to SpawnTimer function

--- Assets/Runtime/Common/Timer/TimerExtensions.cs
+++ Assets/Runtime/Common/Timer/TimerExtensions.cs
@@ -1,14 +1,18 @@
+using System;
 using UnityEngine;
 
 public static class TimerExtensions
 {
-    public static Timer SpawnTimer(this MonoBehaviour self, float waitTime, bool oneShot = true)
+    public static Timer SpawnTimer(this MonoBehaviour self, float waitTime, Action onTimeout = null, bool oneShot = true)
     {
         var holder = new GameObject("Timer");
         holder.hideFlags = HideFlags.HideInHierarchy;
         holder.transform.parent = self.transform;
         var timer = holder.AddComponent<Timer>();
         timer.WaitTime = waitTime;
+
+        if (onTimeout != null)
+            timer.Timeout += onTimeout;
 
         if (oneShot)
         {
